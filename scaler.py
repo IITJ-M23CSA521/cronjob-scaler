17467b5bb166e012bb88241da02fc4f017237e2f
Om Solanki <om.solanki@zevo360.com>
2025-04-07 21:48:28 +0530

    initial files

---
+++ scaler.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from kubernetes import client, config
+
+# Load kube config (inside the cluster)
+config.load_incluster_config()
+
+# Kubernetes API client
+api = client.AppsV1Api()
+
+# Call your ML model API to get desired number of replicas
+response = requests.get("http://localhost:5000/predict")
+predicted_replicas = int(response.json().get("replicas", 2))
+
+# Patch the deployment
+deployment_name = "model-service"
+namespace = "default"
+
+patch = {
+    "spec": {
+        "replicas": predicted_replicas
+    }
+}
+
+api.patch_namespaced_deployment(
+    name=deployment_name,
+    namespace=namespace,
+    body=patch
+)
+
+print(f"Scaled {deployment_name} to {predicted_replicas} replicas.")
